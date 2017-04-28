a8372e370acb1875432a69851900a3e123cc7131
dmuffler <dmuffler@uw.edu>
2017-04-27 17:41:16 -0700

    Worked runShell into the program. Additional testing is needed before submission.

--- mash.c
+++ mash.c
@@ -1,12 +1,34 @@
+/* 	Authors: Donald Muffler, Josiah Hopkins
+	Date: 4/27/17
+*/
+
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "unistd.h"
 #include "string.h"
 #include "sys/wait.h"
 
-#define COMMAND_COUNT 3
-#define MAX_ARGUMENT_COUNT 5
-#define MAX_INPUT 255
+// amount of commands in the shell.
+static const int COMMAND_COUNT = 3;
+
+// the arg count per command.
+static const int MAX_ARGUMENT_COUNT = 5;
+
+// max user input size for commands/arguments.
+static const int MAX_INPUT = 255;
+
+// the first command.
+static const int COMMAND_ONE = 0;
+
+// the second command.
+static const int COMMAND_TWO = 1;
+
+// the third command.
+static const int COMMAND_THREE = 2;
+
+// the start of command arguments.
+static const int ARGUMENT_START = 0;
 
 
 // builds the command array.
@@ -65,6 +87,57 @@ void addFileToCommands(char* commandHolder[COMMAND_COUNT][MAX_ARGUMENT_COUNT + 1
 	}
 }
 
+// the shell that branches and runs 3 commands in parallel.
+void runShell(char* commandHolder[COMMAND_COUNT][MAX_ARGUMENT_COUNT + 1]) {
+	  
+	// container to hold pid in order of completion.
+	int processes[COMMAND_COUNT], processCount = 0, processOne, processTwo, processThree;
+
+	processOne = fork();
+
+	// child one
+	if (processOne == 0) {
+		int check = execvp(commandHolder[COMMAND_ONE][ARGUMENT_START],
+							commandHolder[COMMAND_ONE]);
+		printf("[SHELL 1] STATUS CODE=%d\n", check);
+		exit(EXIT_FAILURE);
+	}
+
+	if (processOne > 0) {
+
+		processTwo = fork();
+
+		// child two
+		if (processTwo == 0) {
+			int check = execvp(commandHolder[COMMAND_TWO][ARGUMENT_START],
+								commandHolder[COMMAND_TWO]);
+			printf("[SHELL 2] STATUS CODE=%d\n", check);
+			exit(EXIT_FAILURE);
+		}
+		
+		if (processTwo > 0) {
+
+			processThree = fork();
+
+			// child three
+			if (processThree == 0) {
+				int check = execvp(commandHolder[COMMAND_THREE][ARGUMENT_START],
+									commandHolder[COMMAND_THREE]);
+				printf("[SHELL 3] STATUS CODE=%d\n", check);
+				exit(EXIT_FAILURE);
+			}
+			
+			if (processThree > 0) {
+				processes[processCount++] = wait(NULL);
+				processes[processCount++] = wait(NULL);
+				processes[processCount] = wait(NULL);
+			}
+		}
+	}
+	printf("Done waiting on children: %d %d %d\n", processes[0], processes[1], processes[2]);
+}
+
+
 // frees allocated memory.
 void freeCommandMemory(char* commandHolder[COMMAND_COUNT][MAX_ARGUMENT_COUNT + 1],
 						int actualArgCount[COMMAND_COUNT]) {
@@ -85,144 +158,21 @@ int main(int argc, char *argv[]) {
 
 	// 2d array to hold the commands. 2nd dimension adds 1 to add room for 'NULL'.
 	char* commands[COMMAND_COUNT][MAX_ARGUMENT_COUNT + 1];
-	int p1, p2, p3,p1Status, p2Status, p3Status, actualArgCount[COMMAND_COUNT];
-	
+
+	// holds the amount of arguments per command.
+	int actualArgCount[COMMAND_COUNT];	
 
 	// add 2 for '\n' and '\O'.
 	char filePath[MAX_ARGUMENT_COUNT + 2];
 	
-	// function calls to populate arrays with user input
+	// function calls to populate arrays with user input.
 	buildCommands(commands, actualArgCount);
 	getFilePath(filePath);
 	addFileToCommands(commands, filePath, actualArgCount);
 
-
- 
-	p1 = fork();
-	if(p1 == 0){
-		//child 1
-		execvp(commands[0][0], commands[0]);
-	}
-	if(p1 > 0){
-		//p1 = wait(&p1Status);
-		wait(&p1Status);
-		p2 = fork();
-		if(p2 == 0){
-			//child 2
-			execvp(commands[1][0], commands[1]);
-		}
-		if(p2 > 0){
-			//p1 = wait(&p2Status);
-			wait(&p2Status);
-			p3 = fork();
-			if(p3 == 0){
-				//child 3
-				execvp(commands[2][0], commands[2]);
-			}
-			if(p3 > 0){
-				//parent
-				//Wait for children to finish
-				//p1 = wait(&p3Status);
-				wait(&p3Status);
-			}
-		}
-	}
-	if( access( filePath, F_OK ) != -1 ) {
-   		// file exists
-
-		if (WEXITSTATUS(p1Status)) {
-			printf("[SHELL 1] STATUS CODE=-1\n");
-		}
-
-		if (WEXITSTATUS(p2Status)) {
-			printf("[SHELL 2] STATUS CODE=-1\n");
-		}
-
-		if (WEXITSTATUS(p3Status)) {
-			printf("[SHELL 3] STATUS CODE=-1\n");
-		}		
-	}
-	fprintf(stdout, "Done waiting on children: %d %d %d\n", p1, p2, p3);
-
-
+	runShell(commands);
 
 	freeCommandMemory(commands, actualArgCount);
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// forks and executes three processes. (My practice at completing the assignment.
-//										Use as a reference if you cannot figure
-//										something out or don't). :)
-void runShell(char* commandHolder[COMMAND_COUNT][MAX_ARGUMENT_COUNT + 1]) {
-	  
-	// container to hold pid in order of completion.
-	int processes[3];
-	
-	int processCount = 0;
-	int statusOne;
-	int statusTwo;
-	int statusThree;
-
-
-
-	int processOne = fork();
-
-	if (processOne == 0) {
-		execvp(commandHolder[PROCESS_ONE][ARGUMENT_START], commandHolder[PROCESS_ONE]);
-	}
-
-	if (processOne > 0) {
-
-		int processTwo = fork();
-		processes[processCount++] = wait(&statusOne);
-
-
-		if (processTwo == 0) {
-			execvp(commandHolder[PROCESS_TWO][ARGUMENT_START], commandHolder[PROCESS_TWO]);
-		}
-		
-		if (processTwo > 0) {
-
-			int processThree = fork();
-			processes[processCount++] = wait(&statusTwo);
-
-			if (processThree == 0) {
-				execvp(commandHolder[PROCESS_THREE][ARGUMENT_START], commandHolder[PROCESS_THREE]);
-			}
-			
-			if (processThree > 0) {
-				processes[processCount++] = wait(&statusThree);
-			}
-		}
-
-		if (WEXITSTATUS(statusOne)) {
-			printf("[SHELL 1] STATUS CODE=-1\n");
-		}
-
-		if (WEXITSTATUS(statusTwo)) {
-			printf("[SHELL 2] STATUS CODE=-1\n");
-		}
-
-		if (WEXITSTATUS(statusThree)) {
-			printf("[SHELL 3] STATUS CODE=-1\n");
-		}		
-	}
-
-	printf("Done waiting on children: %d %d %d\n", processes[0], processes[1], processes[2]);
-}*/
